2b31eb6179e6e79000b071099c6addb058548aae
saba200886 <64844643+saba200886@users.noreply.github.com>
2022-07-03 17:12:27 +0400

    realtime firedatabase change listener

--- app/src/main/java/com/sburnadze/final_project_messenger_app/MainActivity.kt
+++ app/src/main/java/com/sburnadze/final_project_messenger_app/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.sburnadze.final_project_messenger_app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +21,19 @@ class MainActivity : AppCompatActivity() {
         val myRef = database.getReference("message")
 
         myRef.setValue("Hello, World!")
+
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
+
+
     }
 }
