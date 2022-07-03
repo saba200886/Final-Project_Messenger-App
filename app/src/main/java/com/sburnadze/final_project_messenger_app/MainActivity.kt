5006925306cac167abb3c3043701012d6eac4532
saba200886 <64844643+saba200886@users.noreply.github.com>
2022-07-03 16:11:34 +0400

    realtime firedatabase initialized

--- app/src/main/java/com/sburnadze/final_project_messenger_app/MainActivity.kt
+++ app/src/main/java/com/sburnadze/final_project_messenger_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sburnadze.final_project_messenger_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
