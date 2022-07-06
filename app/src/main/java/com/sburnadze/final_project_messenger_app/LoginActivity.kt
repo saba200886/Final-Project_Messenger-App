d87c18eabbc6f21e2e952578b9131f082b4873b9
saba200886 <64844643+saba200886@users.noreply.github.com>
2022-07-06 13:48:43 +0400

    first login page is added

--- app/src/main/java/com/sburnadze/final_project_messenger_app/LoginActivity.kt
+++ app/src/main/java/com/sburnadze/final_project_messenger_app/LoginActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
 
         val database = Firebase.database
