7c52145ce5fd1162334b299f3ab4be17a3b5bdf4
jurxa <jalud18@freeuni.edu.ge>
2022-07-13 21:05:41 +0400

    added recyclerView item layouts for both sent and received messages and also recyclerView Adapter for chat recyclerView

--- app/src/main/java/com/sburnadze/final_project_messenger_app/model/User.kt
+++ app/src/main/java/com/sburnadze/final_project_messenger_app/model/User.kt
@@ -4,4 +4,4 @@ import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class User(val name: String? = null, val password: String? = null, val profession: String? = null,
-                val avatar: String? = null)
+                val avatar: String? = null, val id: String?)
---
+++ app/src/main/java/com/sburnadze/final_project_messenger_app/view/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.sburnadze.final_project_messenger_app.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
