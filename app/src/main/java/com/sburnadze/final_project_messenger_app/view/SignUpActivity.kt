65172fab59a58c9f516cd9d17d8e247781d0f928
saba200886 <64844643+saba200886@users.noreply.github.com>
2022-07-27 00:52:12 +0400

    search activity error fix

--- app/src/main/java/com/sburnadze/final_project_messenger_app/view/SignUpActivity.kt
+++ app/src/main/java/com/sburnadze/final_project_messenger_app/view/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.sburnadze.final_project_messenger_app.view
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -17,8 +18,9 @@ class SignUpActivity: AppCompatActivity() {
     private lateinit var image: ImageView
     private lateinit var signUpButton: Button
     private lateinit var authorizationViewModel: AuthorizationModel
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
         initView()
     }
